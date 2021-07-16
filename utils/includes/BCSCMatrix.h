2a92b780d1629dd74a10e036208ace6f61871ce5
Kazem <kazem.cheshmi@gmail.com>
2021-07-16 14:53:09 -0400

    namespace of def.h

--- utils/includes/BCSCMatrix.h
+++ utils/includes/BCSCMatrix.h
@@ -6,54 +6,54 @@
 #define FUSION_BCSCMATRIX_H
 
 #include "def.h"
+namespace sym_lib {
+ class BCSCMatrix {
+  BCSC *M;
+ public:
+  /// finds all the different supernodes / blocks in the matrix
+  int supernodes(CSC *A, int limit, bool isLower);
 
-class BCSCMatrix {
- BCSC *M;
-public:
- /// finds all the different supernodes / blocks in the matrix
- int supernodes(CSC *A, int limit, bool isLower);
+  /// calculate the total number of nonzero in the matrix including zero padding
+  int calcSize(CSC *A);
 
- /// calculate the total number of nonzero in the matrix including zero padding
- int calcSize(CSC *A);
+  /// Fill in the row and numerical values for blocked matrix
+  void createFormat(CSC *A);
 
- /// Fill in the row and numerical values for blocked matrix
- void createFormat(CSC *A);
+  // Extract a supernodal CSC from a BCSC
+  CSC *compressed_BCSC_to_CSC();
 
- // Extract a supernodal CSC from a BCSC
- CSC *compressed_BCSC_to_CSC();
+  /// Generates the BCSC arrays from A
+  /// \param nA
+  /// \param A
+  void generateBCSC(CSC *A) {
+   M->nnz = calcSize(A);
+   M->i = new int[M->nnz]();
+   M->x = new double[M->nnz]();
+   M->nrows = new int[M->nodes]();
+   createFormat(A);
+  }
 
- /// Generates the BCSC arrays from A
- /// \param nA
- /// \param A
- void generateBCSC(CSC *A) {
-  M->nnz = calcSize(A);
-  M->i = new int[M->nnz]();
-  M->x = new double[M->nnz]();
-  M->nrows = new int[M->nodes]();
-  createFormat(A);
- }
+ public:
+  BCSCMatrix(CSC *A) {
+   M = new BCSC(A);
+   M->nodes = supernodes(A, A->n, true);
+   generateBCSC(A);
+  }
 
-public:
- BCSCMatrix(CSC *A) {
-  M = new BCSC(A);
-  M->nodes = supernodes(A, A->n, true);
-  generateBCSC(A);
- }
+  BCSCMatrix(CSC *A, int nodes, int *supernodes) {
+   M = new BCSC(A);
+   M->nodes = nodes;
+   M->supernodes = new int[nodes + 1]();
+   std::memcpy(M->supernodes, supernodes, sizeof(int) * (nodes + 1));
+   generateBCSC(A);
+  }
 
- BCSCMatrix(CSC *A, int nodes, int *supernodes) {
-  M = new BCSC(A);
-  M->nodes = nodes;
-  M->supernodes = new int[nodes+1]();
-  std::memcpy(M->supernodes, supernodes, sizeof(int) * (nodes+1));
-  generateBCSC(A);
- }
+  BCSC *getBCSC() { return M; }
 
- BCSC *getBCSC() { return M; }
-
- ~BCSCMatrix() {
-  delete(M);
- }
-};
-
+  ~BCSCMatrix() {
+   delete (M);
+  }
+ };
+}
 
 #endif //FUSION_BCSCMATRIX_H
--- utils/includes/def.h
+++ utils/includes/def.h
@@ -13,7 +13,7 @@
 #include <queue>
 #include <cstring>
 
-
+namespace sym_lib {
 #define NULLPNTR nullptr
 #define EMPTY -1
 
@@ -35,247 +35,249 @@
 
 /// Measuring time of a kernel
 ///
-struct timing_measurement {
- double elapsed_time;
- std::chrono::time_point<std::chrono::system_clock> start_time, end_time;
- std::chrono::duration<double> elapsed_seconds;
- std::vector<std::chrono::time_point<std::chrono::system_clock> > t_array;
+ struct timing_measurement {
+  double elapsed_time;
+  std::chrono::time_point<std::chrono::system_clock> start_time, end_time;
+  std::chrono::duration<double> elapsed_seconds;
+  std::vector<std::chrono::time_point<std::chrono::system_clock> > t_array;
 
- timing_measurement() : elapsed_time(0) {}
- ~timing_measurement()= default;
+  timing_measurement() : elapsed_time(0) {}
 
- void start_timer() {
-  start_time = std::chrono::system_clock::now();
-  t_array.push_back(start_time);
- }
+  ~timing_measurement() = default;
 
- double measure_elapsed_time() {
-  assert(t_array.size() > 0);
-  end_time = std::chrono::system_clock::now();
-  elapsed_seconds = end_time - t_array[0];
-  elapsed_time = elapsed_seconds.count();
-  t_array.push_back(end_time);
-  return elapsed_time;
- }
-
- void reset (){
-  elapsed_time=0; t_array.clear();
- }
-
- void print_t_array() {
-  for (unsigned long i = 1; i < t_array.size(); ++i) {
-   std::chrono::duration<double> et = t_array[i] - t_array[i - 1];
-   PRINT_CSV(et.count());
+  void start_timer() {
+   start_time = std::chrono::system_clock::now();
+   t_array.push_back(start_time);
   }
- }
 
-};
+  double measure_elapsed_time() {
+   assert(t_array.size() > 0);
+   end_time = std::chrono::system_clock::now();
+   elapsed_seconds = end_time - t_array[0];
+   elapsed_time = elapsed_seconds.count();
+   t_array.push_back(end_time);
+   return elapsed_time;
+  }
+
+  void reset() {
+   elapsed_time = 0;
+   t_array.clear();
+  }
+
+  void print_t_array() {
+   for (unsigned long i = 1; i < t_array.size(); ++i) {
+    std::chrono::duration<double> et = t_array[i] - t_array[i - 1];
+    PRINT_CSV(et.count());
+   }
+  }
+
+ };
 
 /// The struct for storing CSC or CSR format
 ///
-struct CSC {
- size_t m; // rows
- size_t n; // columns
- size_t nnz; // nonzeros
- int stype;
- bool is_pattern;
- bool pre_alloc; //if memory is allocated somewhere other than const.
- int *p; // Column pointer array
- int *i; // Row index array
- double *x;
+ struct CSC {
+  size_t m; // rows
+  size_t n; // columns
+  size_t nnz; // nonzeros
+  int stype;
+  bool is_pattern;
+  bool pre_alloc; //if memory is allocated somewhere other than const.
+  int *p; // Column pointer array
+  int *i; // Row index array
+  double *x;
 
- CSC(size_t M, size_t N, size_t NNZ) : m(M), n(N), nnz(NNZ) {
-  is_pattern = false;
-  pre_alloc = false;
-  if (N > 0)
-   p = new int[N + 1]();
-  else
-   p = NULLPNTR;
-  if (NNZ > 0) {
-   i = new int[NNZ]();
-   x = new double[NNZ]();
-  } else {
-   i = NULLPNTR;
-   x = NULLPNTR;
-  }
-  stype = 0;
- };
-
- CSC(size_t M, size_t N, size_t NNZ, bool ip) :
-   m(M), n(N), nnz(NNZ), is_pattern(ip) {
-  is_pattern = ip;
-  pre_alloc = false;
-  if (N > 0)
-   p = new int[N + 1]();
-  else
-   p = NULLPNTR;
-  if (NNZ > 0) {
-   i = new int[NNZ]();
-   if (!is_pattern)
-    x = new double[NNZ]();
+  CSC(size_t M, size_t N, size_t NNZ) : m(M), n(N), nnz(NNZ) {
+   is_pattern = false;
+   pre_alloc = false;
+   if (N > 0)
+    p = new int[N + 1]();
    else
+    p = NULLPNTR;
+   if (NNZ > 0) {
+    i = new int[NNZ]();
+    x = new double[NNZ]();
+   } else {
+    i = NULLPNTR;
     x = NULLPNTR;
-  } else {
-   i = NULLPNTR;
-   x = NULLPNTR;
-  }
-  stype = 0;
- };
+   }
+   stype = 0;
+  };
 
- CSC(size_t M, size_t N, size_t NNZ, int *Ap, int *Ai, double *Ax) {
-  is_pattern = false;
-  pre_alloc = true;
-  m = M;
-  n = N;
-  nnz = NNZ;
-  p = Ap;
-  i = Ai;
-  x = Ax;
- }
-
- CSC(size_t M, size_t N, size_t NNZ, int *Ap, int *Ai, int st) {
-  is_pattern = true;
-  pre_alloc = true;
-  m = M;
-  n = N;
-  nnz = NNZ;
-  p = Ap;
-  i = Ai;
-  x = NULLPNTR;
-  stype = st;
- }
-
- ~CSC() {
-  if(!pre_alloc){
-   if (n > 0)
-    delete[]p;
-   if (nnz > 0) {
-    delete[]i;
+  CSC(size_t M, size_t N, size_t NNZ, bool ip) :
+    m(M), n(N), nnz(NNZ), is_pattern(ip) {
+   is_pattern = ip;
+   pre_alloc = false;
+   if (N > 0)
+    p = new int[N + 1]();
+   else
+    p = NULLPNTR;
+   if (NNZ > 0) {
+    i = new int[NNZ]();
     if (!is_pattern)
-     delete[]x;
+     x = new double[NNZ]();
+    else
+     x = NULLPNTR;
+   } else {
+    i = NULLPNTR;
+    x = NULLPNTR;
+   }
+   stype = 0;
+  };
+
+  CSC(size_t M, size_t N, size_t NNZ, int *Ap, int *Ai, double *Ax) {
+   is_pattern = false;
+   pre_alloc = true;
+   m = M;
+   n = N;
+   nnz = NNZ;
+   p = Ap;
+   i = Ai;
+   x = Ax;
+  }
+
+  CSC(size_t M, size_t N, size_t NNZ, int *Ap, int *Ai, int st) {
+   is_pattern = true;
+   pre_alloc = true;
+   m = M;
+   n = N;
+   nnz = NNZ;
+   p = Ap;
+   i = Ai;
+   x = NULLPNTR;
+   stype = st;
+  }
+
+  ~CSC() {
+   if (!pre_alloc) {
+    if (n > 0)
+     delete[]p;
+    if (nnz > 0) {
+     delete[]i;
+     if (!is_pattern)
+      delete[]x;
+    }
    }
   }
- }
 
-};
-
-struct CSR {
- size_t m; // rows
- size_t n; // columns
- size_t nnz; // nonzeros
- int stype;
- bool is_pattern;
- bool pre_alloc; //if memory is allocated somewhere other than const.
- int *p; // Row pointer array
- int *i; // Column index array
- double *x;
-
- CSR(size_t M, size_t N, size_t NNZ) : m(M), n(N), nnz(NNZ) {
-  is_pattern = false;
-  pre_alloc=false;
-  if (M > 0)
-   p = new int[M + 1]();
-  else
-   p = NULLPNTR;
-  if (NNZ > 0) {
-   i = new int[NNZ]();
-   x = new double[NNZ]();
-  } else {
-   i = NULLPNTR;
-   x = NULLPNTR;
-  }
-  stype = 0;
- }
-
- CSR(size_t M, size_t N, size_t NNZ, bool ip) :
-   m(M), n(N), nnz(NNZ), is_pattern(ip) {
-  is_pattern = ip;
-  pre_alloc=false;
-  if (M > 0)
-   p = new int[M + 1]();
-  else
-   p = NULLPNTR;
-  if (NNZ > 0) {
-   i = new int[NNZ]();
-   if (!is_pattern)
-    x = new double[NNZ]();
-   else
-    x = NULLPNTR;
-  } else {
-   i = NULLPNTR;
-   x = NULLPNTR;
-  }
-  stype = 0;
  };
 
- CSR(size_t M, size_t N, size_t NNZ, int *Ap, int *Ai, int st) {
-  is_pattern = true;
-  pre_alloc = true;
-  m = M;
-  n = N;
-  nnz = NNZ;
-  p = Ap;
-  i = Ai;
-  x = NULLPNTR;
-  stype = st;
- }
+ struct CSR {
+  size_t m; // rows
+  size_t n; // columns
+  size_t nnz; // nonzeros
+  int stype;
+  bool is_pattern;
+  bool pre_alloc; //if memory is allocated somewhere other than const.
+  int *p; // Row pointer array
+  int *i; // Column index array
+  double *x;
 
- ~CSR() {
-  if(!pre_alloc){
-   if (m > 0)
-    delete[]p;
-   if (nnz > 0) {
-    delete[]i;
-    delete[]x;
+  CSR(size_t M, size_t N, size_t NNZ) : m(M), n(N), nnz(NNZ) {
+   is_pattern = false;
+   pre_alloc = false;
+   if (M > 0)
+    p = new int[M + 1]();
+   else
+    p = NULLPNTR;
+   if (NNZ > 0) {
+    i = new int[NNZ]();
+    x = new double[NNZ]();
+   } else {
+    i = NULLPNTR;
+    x = NULLPNTR;
+   }
+   stype = 0;
+  }
+
+  CSR(size_t M, size_t N, size_t NNZ, bool ip) :
+    m(M), n(N), nnz(NNZ), is_pattern(ip) {
+   is_pattern = ip;
+   pre_alloc = false;
+   if (M > 0)
+    p = new int[M + 1]();
+   else
+    p = NULLPNTR;
+   if (NNZ > 0) {
+    i = new int[NNZ]();
+    if (!is_pattern)
+     x = new double[NNZ]();
+    else
+     x = NULLPNTR;
+   } else {
+    i = NULLPNTR;
+    x = NULLPNTR;
+   }
+   stype = 0;
+  };
+
+  CSR(size_t M, size_t N, size_t NNZ, int *Ap, int *Ai, int st) {
+   is_pattern = true;
+   pre_alloc = true;
+   m = M;
+   n = N;
+   nnz = NNZ;
+   p = Ap;
+   i = Ai;
+   x = NULLPNTR;
+   stype = st;
+  }
+
+  ~CSR() {
+   if (!pre_alloc) {
+    if (m > 0)
+     delete[]p;
+    if (nnz > 0) {
+     delete[]i;
+     delete[]x;
+    }
    }
   }
- }
 
-};
+ };
 
 
-struct BCSC {
- size_t m; // rows
- size_t n; // cols
- size_t nnz;
- int *p;
- int *i;
- double *x;
+ struct BCSC {
+  size_t m; // rows
+  size_t n; // cols
+  size_t nnz;
+  int *p;
+  int *i;
+  double *x;
 
- // block format specifics
- size_t nodes;
- int *supernodes;
- int *nrows;
+  // block format specifics
+  size_t nodes;
+  int *supernodes;
+  int *nrows;
 
- BCSC (CSC *A) : m(A->m), n(A->n) {
-  assert(A != nullptr);
- }
+  BCSC(CSC *A) : m(A->m), n(A->n) {
+   assert(A != nullptr);
+  }
 
- ~BCSC() {
-  delete[]p;
-  delete[]i;
-  delete[]x;
-  delete[]supernodes;
-  delete[]nrows;
- }
-};
+  ~BCSC() {
+   delete[]p;
+   delete[]i;
+   delete[]x;
+   delete[]supernodes;
+   delete[]nrows;
+  }
+ };
 
-struct Dense {
- size_t row;
- size_t col;
- size_t lda;
- double *a;
+ struct Dense {
+  size_t row;
+  size_t col;
+  size_t lda;
+  double *a;
 
- Dense(size_t M, size_t N, size_t LDA) : row(M), col(N), lda(LDA) {
-  assert(lda == 1 || lda == N);
-  if (row > 0 && col > 0)
-   a = new double[row * col]();
- }
+  Dense(size_t M, size_t N, size_t LDA) : row(M), col(N), lda(LDA) {
+   assert(lda == 1 || lda == N);
+   if (row > 0 && col > 0)
+    a = new double[row * col]();
+  }
 
- ~Dense() {
-  if (row > 0 && col > 0)
-   delete[]a;
- }
-};
-
+  ~Dense() {
+   if (row > 0 && col > 0)
+    delete[]a;
+  }
+ };
+}
 #endif //PROJECT_DEF_H
